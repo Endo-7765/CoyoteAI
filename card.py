002f48d9fdddc1798e974cdb8f2ba1d34ab1b22c
Kazuharu Endo <endo@mi.t.u-tokyo.ac.jp>
2019-12-02 16:54:23 +0900

    implement __str__ in card.py

--- card.py
+++ card.py
@@ -3,3 +3,12 @@ class Card:
     def __init__(self, num, flag):
         self.num = num
         self.flag = flag
+    def __str__(self):
+        if self.flag == 0:
+            return str(self.num)
+        elif self.flag == 1:
+            return "x2"
+        elif self.flag == 2:
+            return "max0"
+        else:
+            return "?"
